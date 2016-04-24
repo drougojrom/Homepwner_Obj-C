7fcf72b42c799779cb489176ea35df251d028624
Roman Ustuantsev <liberrom.hse@gmail.com>
2016-04-24 23:25:44 +0300

    added initial files

---
+++ Homepwner_Obj-C/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Homepwner_Obj-C
+//
+//  Created by Roman Ustiantcev on 24/04/16.
+//  Copyright © 2016 Roman Ustiantcev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
